1dc6c7dc1b90c3e64908fb6a3a61d747b4604ef3
EngineerSource <88381129+EngineerSource@users.noreply.github.com>
2021-09-17 03:46:31 +0300

    Add files via upload

--- Engineer.lua
+++ Engineer.lua
@@ -3065,6 +3065,17 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/ba8lawa&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
+if text == 'ضياء' or text == 'المطور' then
+Text = "[⦑ وياك ضياء تفضل ⦒](t.me/dia2i)\n[⦑ القناة الخاصة ⦒](t.me/M10_Z5)\n"
+keyboard = {} 
+keyboard.inline_keyboard = {
+{{text = 'ضــــيَـــإآءء',url="https://t.me/dia2i"},
+{text = 'قـ♥̨̥̬̩ــنــآإتـ♥̨̥̬̩ـي',url="https://t.me/M10_Z5"}},
+}
+local msg_id = msg.id_/2097152/0.5
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/dia2i&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+end
+
 --------------------------------------------------------------------------------------------------------------
 if Chat_Type == 'GroupBot' and ChekAdd(msg.chat_id_) == true then
 if text == 'رفع ملف السورس' or text == 'رفع الملف' and DevEngineer(msg) then   
